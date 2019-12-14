7c2bf5fb95b35a5b60936b014425913dd749bf8e
monica <mogimartinez@gmail.com>
2019-12-13 22:33:22 -0600

    checks if letter is correct

--- assets/script/game.js
+++ assets/script/game.js
@@ -2,14 +2,71 @@ const characters = ['dobby', 'harry', 'hermoine', 'ron', 'hedwig', 'draco', 'dum
 let wins = 0;
 let losses = 0;
 let guesses = [];
-let guessesRemaining = 0;
-let word = characters[Math.floor(Math.random() * characters.length)];
+let guessesRemaining = 9;
 let answer = [];
 
+//Loops through word and displays underscores instead of actual letters
+let word = characters[Math.floor(Math.random() * characters.length)];
 
-for(let i = 0; i < word.length; i++){
-    answer[i] = '_'
+for (let i = 0; i < word.length; i++) {
+    answer[i] = "_";
+}
+// console.log(word);
+
+
+//Starts/restarts game
+function startGame() {
+    guessesRemaining = 9;
+    guesses = [];
+
+    //Displays Wins
+    document.getElementById('wins').innerHTML = wins;
+
+    //Displays Losses
+    document.getElementById('losses').innerHTML = losses;
+
+    //Displays answer 
+    document.getElementById('character').innerHTML = answer;
+
+    //Displays letters guessed
+    document.getElementById('guessed').innerHTML = guesses;
+
+    //displays guesses remaining
+    document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
+
 }
 
+function letterIsCorrect(letter) {
+    //checks if letter is in word
+    let letterInWord = false;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            letterInWord = true;
+        }
+    }
 
-document.getElementById('character').innerHTML = answer;
+    //checks location of letter in array
+    if (letterInWord) {
+        for (let j = 0; j < word.length; j++) {
+            if (word[j] === letter) {
+                answer[j] = letter;
+            }
+        }
+        console.log(answer);
+    }
+}
+
+startGame();
+
+//Captures key clicks
+document.onkeyup = function (event) {
+    // Check if the key pressed is a letter.
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        // Converts all key clicks to lowercase letters.
+        let letterGuessed = event.key.toLowerCase();
+        console.log(letterGuessed);
+        //Checks if letter is Correct
+        letterIsCorrect(letterGuessed);
+   
+    }
+}
